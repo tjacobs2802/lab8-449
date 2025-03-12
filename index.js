8075d9b14fdc13a7fa0214b46b35cbaed861a4d9
Trevor Jacobs <117475995+tjacobs2802@users.noreply.github.com>
2025-03-12 23:14:29 +0000

    it works

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "https://esm.sh/@supabase/supabase-js"
+
 const supabaseUrl = 'https://wubtzzckkvtenqdlicfw.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind1YnR6emNra3Z0ZW5xZGxpY2Z3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE3MDgzMzAsImV4cCI6MjA1NzI4NDMzMH0.8xFGapGUO_MyuMZLadFiCK77f-UZkjFalwMJKETAGYE'
 const supabase = createClient(supabaseUrl, supabaseKey)
 
-let { data: books, error } = await supabase
+
+async function getBooks(){
+  let { data: books, error } = await supabase
   .from('books')
   .select('*')
 
-for(let book of books){
+  for(let book of books){
     let bookList = document.getElementById('books');
-    bookList.innerHTML += `<li>${book.title}</li>`;
+    bookList.innerHTML += `<li>${book.title}</li>`
+  }
 }
+
+getBooks();
